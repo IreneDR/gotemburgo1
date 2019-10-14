1ab43a4da981f139dbf3d796e41e02307ded9d75
IreneDR <idelgado2@us.es>
2019-10-14 17:04:39 +0200

    Starting watershed generic

--- cropWell.m
+++ cropWell.m
@@ -1,13 +1,14 @@
 %% Process images to get only the well and crop it from all the images
 
-frameFiles = dir('First_ExperimentImageSequence\Position_*');
-img_original = {};
+frameFiles = dir('data\input\Position_*');
+gfpFiles= dir('data/gfp/gfp_*');
 mkdir(fullfile(frameFiles(1).folder, 'output'));
-
+mkdir(fullfile(gfpFiles(1).folder, 'output'));
 allBBox = [];
-for numFrame = 1:length(frameFiles)
-     img_original{numFrame, 1} = imread(fullfile(frameFiles(numFrame).folder, frameFiles(numFrame).name));
-     img_bin = imbinarize(img_original{numFrame});
+for timePoint = 1:length(frameFiles)
+     img_original = imread(fullfile(frameFiles(timePoint).folder, frameFiles(timePoint).name));
+     img_gfp= imread(fullfile(gfpFiles(timePoint).folder, gfpFiles(timePoint).name));
+     img_bin = imbinarize(img_original);
      %circularPoints = imfill(imgradient(img_original{numFrame}/65536, 'central')>0, 'holes');
      %img_bin(circularPoints) = 0;
      img_bin_dilated = imdilate(img_bin, strel('disk', 2));
@@ -16,14 +17,17 @@ for numFrame = 1:length(frameFiles)
      img_onlyMiddleCircle = bwareafilt(bwmorph(img_bin_dilated, 'hbreak'), 1, 8);
      bbox = regionprops(img_onlyMiddleCircle, 'boundingbox');
      if isempty(allBBox) == 0
-         difference = allBBox(numFrame-1, :) - bbox.BoundingBox(3:4);
+         difference = allBBox(timePoint-1, :) - bbox.BoundingBox(3:4);
          if sum(difference) > 10
-             disp('Error');
-             [~, bbox.BoundingBox] = imcrop(img_original{numFrame, 1});
+                
+                disp(['Error ' num2str(timePoint)]);
+             [~, bbox.BoundingBox] = imcrop(img_original);
          end
      end
-     allBBox(numFrame, :) = bbox.BoundingBox(3:4);
-     img_original{numFrame, 2} = imcrop(img_original{numFrame}, bbox.BoundingBox);
-     imwrite(img_original{numFrame, 2}, fullfile(frameFiles(numFrame).folder, 'output', frameFiles(numFrame).name));
+     allBBox(timePoint, :) = bbox.BoundingBox(3:4);
+     croppedImage = imcrop(img_original, bbox.BoundingBox);
+     croppedgfp= imcrop(img_gfp, bbox.BoundingBox);
+     imwrite(croppedImage, fullfile(frameFiles(timePoint).folder, 'output', frameFiles(timePoint).name));
+       imwrite(croppedgfp, fullfile(gfpFiles(timePoint).folder, 'output', gfpFiles(timePoint).name));
 end
 
